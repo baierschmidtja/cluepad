2a534a02f213f48133c35a1f4ae5135558598fc9
baierschmidtja <john.a.baierschmidt@gmail.com>
2021-02-28 23:21:00 -0500

    Save notes to LocalStorage

--- src/CluePad.elm
+++ src/CluePad.elm
@@ -1,16 +1,22 @@
-module CluePad exposing (..)
+-- Ports can only be used in a port module
+port module CluePad exposing (..)
 
 import Browser
 import Html exposing (Attribute, Html, button, div, form, input, label, text, br, h4, h5)
 import Html.Attributes exposing (type_, class, id, for, value)
 import Html.Events exposing (keyCode, on, onBlur, onClick, onFocus, onInput)
+import Json.Encode as JsonE
+import Json.Decode as JsonD
+
 
 -- MAIN
+
+
 main =
     Browser.element
     { init = init
-    , update = update
-    , subscriptions = subscriptions
+    , update = updateWithStorage
+    , subscriptions = \_ -> Sub.none
     , view = view 
     }
 
@@ -20,6 +26,14 @@ type ItemCategory =
   Character
   | Room
   | Weapon
+
+itemCategoryToString : ItemCategory -> String
+itemCategoryToString itemCategory =
+  case itemCategory of
+    Character -> "Character"
+    Room -> "Room"        
+    Weapon -> "Weapon"
+
 
 type GameObjectId
   = None
@@ -44,6 +58,7 @@ type GameObjectId
   | Library
   | Lounge
   | Study
+
 
 gameObjectIdToString : GameObjectId -> String
 gameObjectIdToString gameObjectId =
@@ -114,12 +129,14 @@ gameObjectIdToString gameObjectId =
      Study ->
        "Study"
 
+
 type alias Item =
   { category: ItemCategory
   , gameObjectId: GameObjectId
   , name: String
   , note: String
   }
+
 
 type alias Model =
   { items: List Item
@@ -131,44 +148,57 @@ isItemInCategory : ItemCategory -> Item -> Bool
 isItemInCategory itemCategory item =
   item.category == itemCategory
 
+
 getItemsForCategory : ItemCategory -> List Item -> List Item
 getItemsForCategory itemCategory items = 
   List.filter (isItemInCategory itemCategory) items
 
 
+-- Here we use "flags" to load information in from localStorage. The
+-- data comes in as a JS value, so we define a `decoder` at the bottom
+-- of this file to turn it into an Elm value.
+--
+-- Check out index.html to see the corresponding code on the JS side.
+--
+init : JsonE.Value -> (Model, Cmd Msg)
+init flags =
+  let
+    model = Model
+      [ Item Character MissScarlett "Miss Scarlett" ""
+      , Item Character ColMustard "Col. Mustard" ""
+      , Item Character MrsWhite "Mrs. White" ""
+      , Item Character MrGreen "Mr. Green" ""
+      , Item Character MrsPeacock "Mrs. Peacock" ""
+      , Item Character ProfPlum "Prof. Plum" ""
+      , Item Room Ballroom "Ballroom" ""
+      , Item Room BilliardRoom "Billiard Room" ""
+      , Item Room Conservatory "Conservatory" ""
+      , Item Room DiningRoom "Dining Room" ""
+      , Item Room Hall "Hall" ""
+      , Item Room Kitchen "Kitchen" ""
+      , Item Room Library "Library" ""
+      , Item Room Lounge "Lounge" ""
+      , Item Room Study "Study" ""
+      , Item Weapon Candlestick "Candlestick" ""
+      , Item Weapon Knife "Knife" ""
+      , Item Weapon LeadPipe "Lead Pipe" ""
+      , Item Weapon Revolver "Revolver" ""
+      , Item Weapon Rope "Rope" ""
+      , Item Weapon Wrench "Wrench" ""
+      ]
+      None
+      Character
+  in
+    ( case JsonD.decodeValue serializableNotesDecoder flags of
+         Ok serializableNotes -> { model | items = mapSerializableNotesToItems serializableNotes model.items }
+         Err _ -> model
+    , Cmd.none
+    )
 
-
-init : () -> (Model, Cmd Msg)
-init _ =
-  ( Model [ Item Character MissScarlett "Miss Scarlett" ""
-    , Item Character ColMustard "Col. Mustard" ""
-    , Item Character MrsWhite "Mrs. White" ""
-    , Item Character MrGreen "Mr. Green" ""
-    , Item Character MrsPeacock "Mrs. Peacock" ""
-    , Item Character ProfPlum "Prof. Plum" ""
-    , Item Room Ballroom "Ballroom" ""
-    , Item Room BilliardRoom "Billiard Room" ""
-    , Item Room Conservatory "Conservatory" ""
-    , Item Room DiningRoom "Dining Room" ""
-    , Item Room Hall "Hall" ""
-    , Item Room Kitchen "Kitchen" ""
-    , Item Room Library "Library" ""
-    , Item Room Lounge "Lounge" ""
-    , Item Room Study "Study" ""
-    , Item Weapon Candlestick "Candlestick" ""
-    , Item Weapon Knife "Knife" ""
-    , Item Weapon LeadPipe "Lead Pipe" ""
-    , Item Weapon Revolver "Revolver" ""
-    , Item Weapon Rope "Rope" ""
-    , Item Weapon Wrench "Wrench" ""
-    ]
-    None
-    Character
-  , Cmd.none
-  )
 
 
 -- UPDATE
+
 
 type Msg
   = ItemNoteSelected GameObjectId
@@ -176,6 +206,7 @@ type Msg
   | SaveAllNotes
   | ClearAllNotes
   | SwitchTab ItemCategory
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -206,9 +237,11 @@ update msg model =
       , Cmd.none
       )
 
+
 updateItems : List Item -> GameObjectId -> String -> List Item
 updateItems items gameObjectId note =
   List.map (updateItemNote gameObjectId note) items
+
 
 updateItemNote : GameObjectId -> String -> Item -> Item
 updateItemNote gameObjectId note item =
@@ -217,21 +250,20 @@ updateItemNote gameObjectId note item =
   else
     item
 
+
 clearNote : Item -> Item
 clearNote item =
   { item | note = "" }
+
 
 clearNotes : List Item -> List Item
 clearNotes items = 
   List.map clearNote items
 
--- SUBSCRIPTIONS
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Sub.none
 
 -- VIEW
+
 
 view : Model -> Html Msg
 view model =
@@ -254,9 +286,11 @@ view model =
       ]
     ]
 
+
 getNoteHtmlInputId : Item -> String
 getNoteHtmlInputId item =
   (String.toLower (gameObjectIdToString item.gameObjectId)) ++ "note"
+
 
 viewItem : Item -> Html Msg
 viewItem item =
@@ -269,10 +303,182 @@ viewItems : List Item -> Html Msg
 viewItems items =
   div [] (List.map viewItem items)
 
+
 viewButton : String -> Msg -> String -> Html Msg
 viewButton cls toMsg txt = 
   button [ type_ "button", class cls, onClick toMsg ] [ text txt ]
 
+
 viewItemCategory : ItemCategory -> List Item -> Html Msg
 viewItemCategory itemCategory items =
   viewItems (getItemsForCategory itemCategory items)
+
+
+
+-- PORTS
+
+
+port setLocalStorage : JsonE.Value -> Cmd msg
+
+-- We want to `setLocalStorage` on every update, so this function adds
+-- the setLocalStorage command on each step of the update function.
+--
+-- Check out index.html to see how this is handled on the JS side.
+--
+updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
+updateWithStorage msg oldModel =
+  let
+    ( newModel, cmds ) = update msg oldModel
+  in
+  ( newModel
+  , Cmd.batch [ setLocalStorage (encodeSerializableNotes (itemsToSerializableNotes newModel.items)), cmds ]
+  )
+
+
+
+-- PERSISTENCE
+
+
+type alias SerializableNote =
+  { gameObjectId: GameObjectId
+  , note: String
+  }
+
+
+itemToSerializableNote : Item -> SerializableNote
+itemToSerializableNote item =
+  SerializableNote item.gameObjectId item.note
+
+
+itemsToSerializableNotes : List Item -> List SerializableNote
+itemsToSerializableNotes items =
+  List.map itemToSerializableNote items
+
+
+hasGameObjectId : GameObjectId -> SerializableNote -> Bool
+hasGameObjectId gameObjectId serializableNote =
+  serializableNote.gameObjectId == gameObjectId
+
+mapSerializableNoteToItem : List SerializableNote -> Item -> Item
+mapSerializableNoteToItem serializableNotes item  =
+  let
+    maybeSerializableNote = List.head (List.filter (hasGameObjectId item.gameObjectId) serializableNotes)
+  in
+    case maybeSerializableNote of
+       
+       Maybe.Just serializableNote ->
+         { item | note = serializableNote.note }
+
+       Maybe.Nothing ->
+         item
+
+
+mapSerializableNotesToItems : List SerializableNote -> List Item -> List Item
+mapSerializableNotesToItems serializableNotes items  =
+  List.map (mapSerializableNoteToItem serializableNotes) items
+
+
+
+-- JSON DECODERS
+
+
+gameObjectIdDecoder : JsonD.Decoder GameObjectId
+gameObjectIdDecoder =
+  JsonD.string |> JsonD.andThen (\gameObjectIdStr ->
+    case gameObjectIdStr of
+      "None" ->
+        JsonD.succeed None
+     
+      "MissScarlett" ->
+        JsonD.succeed MissScarlett
+  
+      "ColMustard" ->
+        JsonD.succeed ColMustard 
+  
+      "MrsWhite" ->
+        JsonD.succeed MrsWhite
+  
+      "MrGreen" ->
+        JsonD.succeed MrGreen
+
+      "MrsPeacock" ->
+        JsonD.succeed MrsPeacock
+  
+      "ProfPlum" ->
+        JsonD.succeed ProfPlum
+
+      "Candlestick" ->
+        JsonD.succeed Candlestick
+
+      "Knife" ->
+        JsonD.succeed Knife
+
+      "LeadPipe" ->
+        JsonD.succeed LeadPipe
+  
+      "Revolver" ->
+        JsonD.succeed Revolver
+
+      "Rope" ->
+        JsonD.succeed Rope
+  
+      "Wrench" ->
+        JsonD.succeed Wrench
+
+      "Ballroom" ->
+        JsonD.succeed Ballroom
+
+      "BilliardRoom" ->
+        JsonD.succeed BilliardRoom
+     
+      "Conservatory" ->
+        JsonD.succeed Conservatory
+  
+      "DiningRoom" ->
+        JsonD.succeed DiningRoom
+  
+      "Hall" ->
+        JsonD.succeed Hall
+     
+      "Kitchen" ->
+        JsonD.succeed Kitchen
+
+      "Library" ->
+        JsonD.succeed Library
+
+      "Lounge" ->
+        JsonD.succeed Lounge
+     
+      "Study" ->
+        JsonD.succeed Study
+
+      _ ->
+        JsonD.fail "invalid GameObjectId"
+  
+  )
+
+
+serializableNoteDecoder : JsonD.Decoder SerializableNote
+serializableNoteDecoder =
+  JsonD.map2 SerializableNote
+    (JsonD.field "gameObjectId" gameObjectIdDecoder)
+    (JsonD.field "note" JsonD.string)
+
+
+serializableNotesDecoder : JsonD.Decoder (List SerializableNote)
+serializableNotesDecoder =
+  JsonD.list serializableNoteDecoder
+
+-- JSON ENCODE
+
+encodeSerializableNote : SerializableNote -> JsonE.Value
+encodeSerializableNote serializableNote =
+  JsonE.object
+    [ ("gameObjectId", JsonE.string (gameObjectIdToString serializableNote.gameObjectId))
+    , ("note", JsonE.string serializableNote.note)
+    ]
+
+
+encodeSerializableNotes : List SerializableNote -> JsonE.Value
+encodeSerializableNotes serializableNotes =
+  JsonE.list encodeSerializableNote serializableNotes
